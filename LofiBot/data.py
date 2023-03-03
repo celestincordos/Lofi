e60ead98ceb43b5c58088e645e9ab8ce9c89860e
Celestin Cordos <celestin.cordos@outlook.fr>
2023-03-03 22:35:00 +0100

    hashes might not be unique.... but will consider that they are unique enough for now....

--- LofiBot/data.py
+++ LofiBot/data.py
@@ -1,4 +1,35 @@
 import os
+import numpy as np
+from hashlib import md5
+
+
+class Track:
+
+    def _encode(self) -> str:
+        # maybe here I need mu = list(self.features) and to use mu instead of self.features in the following
+        # create a hash for vector mu
+        hash = ""
+        # first 20 characters are each sampled from 5 entries
+        for i in range(0, 100, 5):
+            hash += str((self.features[0][i:i +
+                        1].abs().sum() * 587).int().item())[-1]
+        # last 4 characters are the beginning of the MD5 hash of the whole vector
+        hash2 = int(md5(self.features.numpy()).hexdigest(), 16)
+        hash = f"#{hash}{hash2}"[:25]
+        return hash
+
+    def __init__(self, features: np.array[np.float32], id: str = None) -> None:
+        self.features: np.array[np.float32] = features
+        # will still have to test this syntax right here... But it should work according to the first tests
+        self.id: str = id or self._encode(self.features)
+
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, Track):
+            raise NotImplemented(
+                f"Cannot compare object of type {type(__o)} and obect of type {type(self)}")
+        if self.id is not None and __o.id is not None:
+            return self.id == __o.id
+        return np.array_equal(self.features, __o.features)
 
 
 class DataManager:
--- LofiBot/orchestrator.py
+++ LofiBot/orchestrator.py
@@ -2,21 +2,6 @@ import asyncio
 
 from creator import Creator
 from data import DataManager
-import numpy as np
-
-
-class Track:
-    def __init__(self, features: np.array[np.float32], id: str = None) -> None:
-        self.features = features
-        self.id = id
-
-    def __eq__(self, __o: object) -> bool:
-        if not isinstance(__o, Track):
-            raise NotImplemented(
-                f"Cannot compare object of type {type(__o)} and obect of type {type(self)}")
-        if self.id is not None and __o.id is not None:
-            return self.id == __o.id
-        return np.array_equal(self.features, __o.features)
 
 
 class Orchestrator:
