bd1d19e3d25b24191a05414ef2f8c4a11b6c1a5a
Celestin Cordos <celestin.cordos@outlook.fr>
2023-04-21 21:50:13 +0200

    Should really be OK now...

--- LofiBot/video.py
+++ LofiBot/video.py
@@ -12,7 +12,7 @@ class Editor:
     def _compile_audio(self, audio_files: list[str], folder_name: str):
         audio_files.sort()
         audio: AudioSegment | None = None
-        logging.log("Compiling audio files...")
+        logging.info("Compiling audio files...")
         for filename in tqdm(audio_files):
             full_filename = os.path.join(folder_name, filename)
             current_track = AudioSegment.from_mp3(full_filename)
@@ -23,14 +23,17 @@ class Editor:
         output_name = os.path.join(folder_name, "audio.mp3")
         audio.export(output_name, format="mp3")
 
-    def _compile_video(self, image_file: str, folder_name: str) -> None:
-        bashCommand = f"ffmpeg -loop 1 -i {os.path.join (folder_name,image_file)} -i {os.path.join(folder_name, 'audio.mp3')} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest {os.path.join(folder_name, 'out.mp4')}"
+    def _compile_video(self, image_file: str, folder_name: str, audio_output: str, video_output: str) -> None:
+        bashCommand = f"ffmpeg -loop 1 -i {os.path.join (folder_name,image_file)} -i {audio_output} -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest {video_output}"
         os.system(bashCommand)
 
-    def _move_folder(folder_name: str) -> None:
-        move(folder_name, EDITS_PATH)
+    def _move_files(self, folder_base_name: str, audio_output: str, video_output: str) -> None:
+        destination = os.path.join(EDITS_PATH, folder_base_name)
+        os.mkdir(destination)
+        move(audio_output, destination)
+        move(video_output, destination)
 
-    def compile_folder(self, folder_name: str):
+    def compile_folder(self, folder_name: str, base_folder_name: str):
         audio_files: list[str] = []
         image_file: str
         all_files = os.listdir(folder_name)
@@ -41,9 +44,13 @@ class Editor:
                 image_file = file
             else:
                 logging.warning("What is file {file} in folder {folder}??")
+        audio_output = os.path.join(folder_name, 'audio.mp3')
+        video_output = os.path.join(folder_name, 'out.mp4')
         self._compile_audio(audio_files=audio_files, folder_name=folder_name)
-        self._compile_video(image_file=image_file, folder_name=folder_name)
-        self._move_folder(folder_name)
+        self._compile_video(image_file=image_file, folder_name=folder_name,
+                            audio_output=audio_output, video_output=video_output)
+        self._move_files(folder_name=base_folder_name,
+                         audio_output=audio_output, video_output=video_output)
         return
 
     def make_edits(self):
@@ -53,7 +60,8 @@ class Editor:
         for i, folder_name in enumerate(dirs):
             logging.info("Compiling folder {i}/{length}")
             full_folder_name = os.path.join(PRODUCED_PATH, folder_name)
-            self.compile_folder(folder_name=full_folder_name)
+            self.compile_folder(folder_name=full_folder_name,
+                                base_folder_name=folder_name)
 
         return
 
